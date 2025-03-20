6c515d099a622736cdf1b87212d0d7d89cecf1a8
LibrasEdna <127267215+LibrasEdna@users.noreply.github.com>
2025-03-20 17:41:41 +0800

    Add files via upload

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Link from "next/link";
+import Hearder from "./components/header";
 
 export const metadata = {
   title: "Create Next App",
@@ -23,40 +24,7 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body>
-        <div className="navbar bg-base-100 shadow-sm">
-          <div className="flex-1">
-            <a className="btn btn-ghost text-xl">Dance School</a>
-          </div>
-          <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/#">Daily Schedule</Link>
-              </li>
-              <li>
-                <Link href="/tutor">Tutor</Link>
-              </li>
-              <li>
-                <Link href="/about">About us</Link>
-              </li>
-              <li>
-                <details>
-                  <summary>Account</summary>
-                  <ul className="bg-base-100 rounded-t-none p-2">
-                    <li>
-                      <Link href="/login">Login</Link>
-                    </li>
-                    <li>
-                      <Link href="/register">Registration</Link>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <Hearder />
         {children}
       </body>
     </html>
